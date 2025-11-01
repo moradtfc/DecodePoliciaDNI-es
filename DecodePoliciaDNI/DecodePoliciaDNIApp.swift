c8307373c1b6534c38c76eb3613681c11511d76e
Jesús Alberto Mora <jesusmora0101@gmail.com>
2025-11-01 20:49:44 +0100

    Initial commit

---
+++ DecodePoliciaDNI/DecodePoliciaDNIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecodePoliciaDNIApp.swift
+//  DecodePoliciaDNI
+//
+//  Created by Jesus Mora on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DecodePoliciaDNIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
